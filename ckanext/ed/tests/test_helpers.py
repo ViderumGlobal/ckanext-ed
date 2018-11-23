0135acae206a7affca09b7e2f8ebef59471a8852
Irakli Mchedlishvili <irakli.mchedlishvili@datopian.com>
2018-11-23 10:29:41 +0400

    [tests][s]: bing back helper tests - refs #7

--- ckanext/ed/tests/test_helpers.py
+++ ckanext/ed/tests/test_helpers.py
@@ -1,62 +1,46 @@
-#  from ckan import plugins
-#  from ckan.tests import helpers as test_helpers
-#  from ckanext.ed import helpers
-#  from ckan.tests import factories
-#  from ckan.lib.search import rebuild
+from ckan import plugins
+from ckan.tests import helpers as test_helpers
+from ckanext.ed import helpers
+from ckan.tests import factories
+from ckan.lib.search import rebuild
 
 
-#  class HelpersBase(object):
-    #  def setup(self):
-        #  test_helpers.reset_db()
+class HelpersBase(object):
+    def setup(self):
+        test_helpers.reset_db()
+        rebuild()
+        if not plugins.plugin_loaded('ed'):
+             plugins.load('ed')
 
-        #  rebuild()
-
-        #  if not plugins.plugin_loaded('ed'):
-            #  plugins.load('ed')
-
-    #  @classmethod
-    #  def teardown_class(self):
-
-        #  if plugins.plugin_loaded('ed'):
-            #  plugins.unload('ed')
+    @classmethod
+    def teardown_class(self):
+        if plugins.plugin_loaded('ed'):
+            plugins.unload('ed')
 
 
-#  class TestHelpers(HelpersBase, test_helpers.FunctionalTestBase):
-    #  def test_get_recently_updated_datasets(self):
-        #  factories.Dataset()
-        #  factories.Dataset()
-        #  factories.Dataset()
-        #  dataset = factories.Dataset()
+class TestHelpers(HelpersBase, test_helpers.FunctionalTestBase):
+    def test_get_recently_updated_datasets(self):
+        factories.Dataset()
+        factories.Dataset()
+        factories.Dataset()
+        dataset = factories.Dataset()
 
-        #  result = helpers.get_recently_updated_datasets()
+        result = helpers.get_recently_updated_datasets()
+        assert len(result) == 4
+        assert result[0]['id'] == dataset['id']
 
-        #  assert len(result) == 4
-        #  assert result[0]['id'] == dataset['id']
+        result = helpers.get_recently_updated_datasets(limit=2)
+        assert len(result) == 2
+        assert result[0]['id'] == dataset['id']
 
-        #  result = helpers.get_recently_updated_datasets(limit=2)
+    def test_get_groups(self):
+        group1 = factories.Group()
 
-        #  assert len(result) == 2
-        #  assert result[0]['id'] == dataset['id']
-
-    #  def test_get_groups(self):
-        #  group1 = factories.Group()
-        #  result = helpers.get_groups()
-
-        #  assert len(result) == 1
-
-        #  factories.Group()
-        #  factories.Group()
-        #  factories.Group()
-
-        #  result = helpers.get_groups()
-
-        #  assert result[0]['id'] == group1['id']
-        #  assert len(result) == 4
-
-
-def test_1():
-    assert True
-
-
-def test_2():
-    assert True
+        result = helpers.get_groups()
+        assert len(result) == 1
+        factories.Group()
+        factories.Group()
+        factories.Group()
+        result = helpers.get_groups()
+        assert result[0]['id'] == group1['id']
+        assert len(result) == 4
