1ac9211057f0226452c07817d71c38efea2acbad
amercader <amercadero@gmail.com>
2018-11-30 13:30:41 +0100

    Fix tests
    call_action sets ignore_auth=True by default, which basically means that
    everybody can `package_update`, so the the check on `package_show`
    doesn't work. Also simplified the tests when possible.

--- ckanext/ed/tests/test_plugins.py
+++ ckanext/ed/tests/test_plugins.py
@@ -1,11 +1,7 @@
 from nose.tools import assert_raises, assert_equals
-from paste.registry import Registry
-import mock
-import pylons
 
-from ckan import model
 from ckan.tests import factories as core_factories
-from ckan.tests.helpers import call_action, call_auth, FunctionalTestBase
+from ckan.tests.helpers import call_action, FunctionalTestBase
 import ckan.plugins.toolkit as toolkit
 
 from ckanext.ed.tests import factories
@@ -20,33 +16,32 @@ class TestPlugins(FunctionalTestBase):
             users=[
                 {'name': 'george', 'capacity': 'admin'},
                 {'name': 'john', 'capacity': 'editor'},
-                {'name': 'paul', 'capacity': 'reader'}
+                {'name': 'paul', 'capacity': 'member'}
             ],
             name='us-ed-1',
             id='us-ed-1'
         )
-        dataset = factories.Dataset(approval_state='approved', owner_org='us-ed-1')
+        factories.Dataset(approval_state='approved', owner_org='us-ed-1')
 
         # Admin
-        context = _create_context({'name': 'george'})
+        context = {'user': 'george'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Editor
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Member
-        context = _create_context({'name': 'paul'})
+        context = {'user': 'paul'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Anonimous
-        context = _create_context({'name': 'ringo'})
+        context = {'user': 'ringo'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
-
 
     def test_dataset_not_appears_in_search_if_not_approved(self):
         core_factories.User(name='george')
@@ -64,39 +59,38 @@ class TestPlugins(FunctionalTestBase):
 
         # Dataset created by factories seem to use sysadmin so approval_state
         # forced to be "approved". Creating packages this way to avoid that
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         data_dict = _create_dataset_dict('test-dataset-1', 'us-ed-2')
         call_action('package_create', context, **data_dict)
 
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         data_dict = _create_dataset_dict('test-dataset-2', 'us-ed-2')
         call_action('package_create', context, **data_dict)
 
         # 2 datasets above "approveal_pending" and 1 below "approved"
-        context = _create_context({'name': 'george'})
+        context = {'user': 'george'}
         data_dict = _create_dataset_dict('test-dataset-3', 'us-ed-2')
         call_action('package_create', context, **data_dict)
 
         # Admin
-        context = _create_context({'name': 'george'})
+        context = {'user': 'george'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Editor
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Member
-        context = _create_context({'name': 'paul'})
+        context = {'user': 'paul'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
 
         # Anonimous
-        context = _create_context({'name': 'ringo'})
+        context = {'user': 'ringo'}
         packages = call_action('package_search', context, **{})
         assert_equals(packages['count'], 1)
-
 
     def test_package_show(self):
         core_factories.User(name='george')
@@ -106,7 +100,7 @@ class TestPlugins(FunctionalTestBase):
             users=[
                 {'name': 'george', 'capacity': 'admin'},
                 {'name': 'john', 'capacity': 'editor'},
-                {'name': 'paul', 'capacity': 'reader'}
+                {'name': 'paul', 'capacity': 'member'}
             ],
             name='us-ed-3',
             id='us-ed-3'
@@ -117,49 +111,45 @@ class TestPlugins(FunctionalTestBase):
 
         # Dataset created by factories seem to use sysadmin so approval_state
         # forced to be "approved". Creating packages this way to avoid that
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         data_dict = _create_dataset_dict(pkg_1, 'us-ed-3')
         call_action('package_create', context, **data_dict)
 
         # 1 datasets above "approveal_pending" and 1 below "approved"
-        context = _create_context({'name': 'george'})
+        context = {'user': 'george'}
         data_dict = _create_dataset_dict(pkg_2, 'us-ed-3')
         call_action('package_create', context, **data_dict)
 
         # Admin
-        context = _create_context({'name': 'george'})
+        context = {'user': 'george'}
         packages = call_action('package_show', context, **{'id': pkg_1})
         assert_equals(packages['name'], pkg_1)
         packages = call_action('package_show', context, **{'id': pkg_2})
         assert_equals(packages['name'], pkg_2)
 
         # Editor
-        context = _create_context({'name': 'john'})
+        context = {'user': 'john'}
         packages = call_action('package_show', context, **{'id': pkg_1})
         assert_equals(packages['name'], pkg_1)
         packages = call_action('package_show', context, **{'id': pkg_2})
         assert_equals(packages['name'], pkg_2)
 
         # 2 checks bellow not passing due to context issue
-        # # Member
-        # context = _create_context({'name': 'paul'})
-        # packages = call_action('package_show', context, **{'id': pkg_1})
-        # assert False, packages
-        # assert_equals(packages, {})
-        # packages = call_action('package_show', context, **{'id': pkg_2})
-        # assert_equals(packages['name'], pkg_2)
-        #
-        # # Anonimous
-        # context = _create_context({'name': 'ringo'})
-        # packages = call_action('package_show', context, **{'id': pkg_1})
-        # assert_equals(packages, {})
-        # packages = call_action('package_show', context, **{'id': pkg_2})
-        # assert_equals(packages['name'], pkg_2)
+        # Member
+        context = {'user': 'paul', 'ignore_auth': False}
+        assert_raises(toolkit.ObjectNotFound,
+                      call_action, 'package_show', context, id=pkg_1)
+        packages = call_action('package_show', context, **{'id': pkg_2})
+        assert_equals(packages['name'], pkg_2)
+
+        # Anonymous
+        context = {'user': 'ringo', 'ignore_auth': False}
+        assert_raises(toolkit.ObjectNotFound,
+                      call_action, 'package_show', context, id=pkg_1)
+        packages = call_action('package_show', context, **{'id': pkg_2})
+        assert_equals(packages['name'], pkg_2)
 
 # Helpers
-
-def _create_context(user):
-    return {'model': model, 'user': user['name']}
 
 
 def _create_dataset_dict(package_name, office_name='us-ed'):
