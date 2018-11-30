5e1c335a9eafaf110aa7b711b34d4a7e52ea0f69
amercader <amercadero@gmail.com>
2018-11-30 13:29:32 +0100

    Make validator logic more robust

--- ckanext/ed/validators.py
+++ ckanext/ed/validators.py
@@ -1,19 +1,22 @@
-from logging import getLogger
-
-import ckan.logic.action.get as get_core
-
-from ckanext.ed import helpers
-
-
-log = getLogger(__name__)
+from ckan.plugins import toolkit
 
 
 def state_validator(key, data, errors, context):
-    user_orgs = get_core.organization_list_for_user(context, {'id': context['user']})
+    user_orgs = toolkit.get_action('organization_list_for_user')(
+        context, {'id': context['user']})
     office_id = data.get(('owner_org',))
-    state = 'approval_pending'
-    # user is member of the org and admin remove pending
+
+    state = data.pop(key, None)
+
+    # If the user is member of the organization but not an admin, force the
+    # state to be pending
     for org in user_orgs:
-        if org.get('id') == office_id and org.get('capacity') == 'admin':
-            state = None
+        if org.get('id') == office_id:
+            if org.get('capacity') == 'admin':
+                # If no state provided and user is an admin, default to active
+                state = state or 'active'
+            else:
+                # If not admin, create as pending
+                state = 'approval_pending'
+
     data[key] = state
